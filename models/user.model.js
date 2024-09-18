ecc9cfdb428ac7edd6fc13c343980873ad5dc5fb
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-09-18 04:15:45 -0700

    job schema done

--- models/user.model.js
+++ models/user.model.js
@@ -70,4 +70,15 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, enteredPassword);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 export const User = mongoose.model("User", userSchema);
