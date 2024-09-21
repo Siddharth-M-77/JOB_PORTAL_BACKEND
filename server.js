6504c1c12d4a3f1f08a236cc139ec5f7de35d60a
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-09-21 09:15:39 -0700

    bug-fix

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import jobRouter from "./routes/job.route.js";
 import companyRouter from "./routes/company.routes.js";
-import applicationRouter from "./routes/apllication.route.js";
-
-dotenv.config({}); // load environment variables
+import applicationRouter from "./routes/application.route.js";
+dotenv.config({}); // Load environment variables
 const app = express();
 
 // Middleware to parse JSON request bodies
@@ -20,21 +19,25 @@ app.use(express.urlencoded({ extended: true }));
 // Serve static files from the "Public" directory
 app.use(express.static("Public"));
 
-// Enable Cross-Origin Resource Sharing
-app.use(cors());
+// Enable Cross-Origin Resource Sharing with specific frontend origin
+const corsOptions = {
+  origin: "https://job-portal-frontend-topaz.vercel.app",
+  credentials: true, // Allow cookies from this origin
+};
+app.use(cors(corsOptions));
 
 // Middleware to parse cookies
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 4000;
 
-//routes
-
+// Routes
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/company", companyRouter);
 app.use("/api/v1/applicants", applicationRouter);
 
+// Connect to MongoDB and start the server
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
